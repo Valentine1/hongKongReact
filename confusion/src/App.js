43df14fe1e8c20a818420644f422012ead4454c7
Yuri Laschuk <devnetdot@gmail.com>
2019-07-16 15:15:36 +0300

    Components Part 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {Navbar, NavbarBrand} from "reactstrap";
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+export default class  App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+
+          </Navbar>
+            <Menu />
         </div>
-      </Navbar>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
