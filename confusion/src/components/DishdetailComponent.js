2496f7b5b0b192dfb1dee7b8c03b3a82eea996c0
Yuri Laschuk <devnetdot@gmail.com>
2019-08-31 15:50:15 +0300

    Functional Components

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,71 +1,66 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle } from 'reactstrap';
 import { ListGroup, ListGroupItem, ListGroupItemText } from 'reactstrap';
 
-class Dishdetail extends Component {
+function RenderDish({dish}) {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedDish: props.selectedDish
-        }
-        console.log("Dishdetail construcor");
-    }
-
-    renderComments() {
-        const comments = this.props.selectedDish.comments.map((comm) => {
-                return <ListGroup key={comm.id}>
-                    <ListGroupItem >
-                        <ListGroupItemText>
-                            {comm.comment}
-                        </ListGroupItemText>
-
-                        <ListGroupItemText>--{comm.author},
-                            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comm.date)))}
-                        </ListGroupItemText>
-                    </ListGroupItem>
-                </ListGroup>
-            }
-        );
-        return (
-            <div>
-                {comments}
-            </div>
-        );
-    }
-
-    render() {
-        console.log("Dishdetail render");
-        if (this.props.selectedDish != null){
-            return (
-                <div  className="row">
-                    <div  className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg top src={this.props.selectedDish.image} alt={this.props.selectedDish.name} />
-                            <CardBody>
-                                <CardTitle>{this.props.selectedDish.name}</CardTitle>
-                                <CardText>{this.props.selectedDish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div  className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardTitle> Comments: </CardTitle>
-                            <CardBody>
-
-                                {this.renderComments()}
-                            </CardBody>
-                        </Card>
-                    </div>
-                </div>
-            );
-        }  else
-            return(
-                <div>I I am null</div>
-            );
-
-    }
+    return (
+    <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+        </CardBody>
+    </Card>
+    )
 }
 
-export default Dishdetail;
+function RenderComments({comments}) {
+    const commentsView = comments.map((comm) => {
+            return <ListGroup key={comm.id}>
+                <ListGroupItem >
+                    <ListGroupItemText>
+                        {comm.comment}
+                    </ListGroupItemText>
+
+                    <ListGroupItemText>--{comm.author},
+                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comm.date)))}
+                    </ListGroupItemText>
+                </ListGroupItem>
+            </ListGroup>
+        }
+    );
+    return (
+        <div >
+            {commentsView}
+        </div>
+    );
+}
+
+const  DishDetail = (props) => {
+    console.log("Dishdetail render");
+    if (props.selectedDish != null){
+        return (
+            <div  className="col-8 col-md-5 m-1">
+                <div  style={{display:'inline'} }>
+                    <RenderDish dish={props.selectedDish} />
+                </div>
+                <div   style={{display:'inline'} }>
+                    <Card>
+                        <CardTitle> Comments: </CardTitle>
+                        <CardBody>
+                            <RenderComments comments={props.selectedDish.comments} />
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
+        );
+    }  else
+        return(
+            <div>I I am null</div>
+        );
+
+}
+
+export default DishDetail;
